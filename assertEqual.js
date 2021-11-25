e2a5951dbf390068ebaee004f9b62783cd1bdf0e
MousumiDutta136 <dutta.mousumi136@gmail.com>
2021-11-24 23:14:03 -0500

    assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -2,11 +2,9 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`📗 Assertion Passed : ${actual} === ${expected}`);
-  }
-  if (actual !== expected) {
+  } else {
     console.log(`🟥 Assertion Failed : ${actual} !== ${expected}`);
   }
-  
 };
 
 // TEST CODE
