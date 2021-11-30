3e0a9d84120f2763bdfea79d6b73501a067e7dba
MousumiDutta136 <dutta.mousumi136@gmail.com>
2021-11-29 22:13:09 -0500

    tailTest.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,19 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
+// const assertEqual = require('../assertEqual');
+// const tail = require('../tail');
 
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]),["Lighthouse", "Labs"]);
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+// assertEqual(tail(["Hello", "Lighthouse", "Labs"]),["Lighthouse", "Labs"]);
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words);
+// assertEqual(words.length, 3);
+
+const assert = require('chai').assert;
+const tail   = require('../tail');
+
+describe("#tail", () => {
+  it("returns [2,3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  // it("returns '5' for ['5']", () => {
+  //   assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), '5'); 
+  // });
+});
